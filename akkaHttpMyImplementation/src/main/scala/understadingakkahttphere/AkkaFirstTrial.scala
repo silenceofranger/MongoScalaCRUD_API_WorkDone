a7fabc7325198ce4c1a7905699459271ef1cd9f5
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2021-01-12 23:38:57 +0530

    update

---
+++ akkaHttpMyImplementation/src/main/scala/understadingakkahttphere/AkkaFirstTrial.scala
@@ -0,0 +1,32 @@
+package understadingakkahttphere
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives.{complete, get, path}
+
+import scala.io.StdIn
+
+object AkkaFirstTrial extends App {
+
+  implicit val system = ActorSystem(Behaviors.empty, "my-system")
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.executionContext
+
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+  val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
+}
